2f6ec739da189d75c3f593053674e54a989cfbf3
Simple002 <meyrzhan04.04@mail.ru>
2025-10-23 20:10:41 +0500

    two

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const cookieParser = require("cookie-parser")
+const jwt = require("jsonwebtoken")
 require("dotenv").config();
 const app = express();
 
@@ -7,9 +10,42 @@ app.use(cors({
     origin:process.env.DOMEN,
     credentials:true,
 }))
+app.use(express.json());
+app.use(cookieParser());
+
+try{
+    mongoose.connect(process.env.MONGO)
+    console.log("Connected MongoDB...")
+}catch{
+    console.log("ERROR WITH CONNECT...")
+    process.exit(1);
+}
+
+const userSchema = mongoose.Schema({
+    name:String,
+})
+
+const User = mongoose.model("User",userSchema);
 
 app.get("/" ,(req,res) => {
     res.json({text:"Shop Card"})
+})
+
+app.post('/api/post',async (req,res) => {
+    const {name} = req.body;
+    const user = new User({name:name});
+    await user.save();
+
+    const token = jwt.sign({name:name},process.env.MONGO);
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24
+    });
+    console.log("Saved")
+
+    res.json({text:"Created..."})
 })
 
 app.listen(3000,() => {
